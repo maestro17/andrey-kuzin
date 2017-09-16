ef3b6494eae7e82bde02fea6047c310ad0eea0ab
maestro17 <kuzya-strelok@yandex.ru>
2017-09-16 18:50:10 +0400

    1.3. JavaDoc. Code convention [#4087]

--- chapter_001/src/main/java/ru/job4j/Calculate.java
+++ chapter_001/src/main/java/ru/job4j/Calculate.java
@@ -1,7 +1,15 @@
 package ru.job4j;
 
+/**
+*Class Calculate, решение задачи из части 001, Базовый синтаксис Java
+*@author Andrey Kuzin
+*@since 15.09.17
+*/
 public class Calculate {
-
+    /**
+    * Метод main
+    * @param выводит строку в консоль
+    */
 	public static void main (String[] argc)
 	{
 		System.out.println ("Hello World\nAuthor Andrey Kuzin");
---
+++ chapter_001/src/main/java/ru/job4j/package-info.java
@@ -0,0 +1,8 @@
+/**
+* Package for calculate task.
+*
+* @author Andrey Kuzin
+* @version 001
+* @since 15.09.17
+*/
+package ru.job4j;
